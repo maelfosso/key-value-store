7a9e411b52bfad57c32be702d995878865427d0d
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-04-16 00:35:18 +0100

    in-memory data storage implemented

--- server.go
+++ server.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 )
+
+var data = map[string]string{}
 
 func main() {
 	// Get port from env variables or set to 8080
@@ -20,6 +24,50 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world.\n"))
+	})
+
+	r.Get("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		data, err := Get(r.Context(), key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		w.Write([]byte(data))
+	})
+
+	r.Delete("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		err := Delete(r.Context(), key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		JSON(w, map[string]string{"status": "success"})
+	})
+
+	r.Post("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		err = Set(r.Context(), key, string(body))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
@@ -36,4 +84,19 @@ func JSON(w http.ResponseWriter, data interface{}) {
 	}
 
 	w.Write(b)
+}
+
+func Set(ctx context.Context, key, value string) error {
+	data[key] = value
+	return nil
+}
+
+func Get(ctx context.Context, key string) (string, error) {
+	return data[key], nil
+}
+
+func Delete(ctx context.Context, key string) error {
+	delete(data, key)
+
+	return nil
 }
