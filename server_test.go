41d842ccf758ca869c3b15fdc32b7ec4f98b3454
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-04-17 01:24:06 +0100

    add test for Get function

--- server_test.go
+++ server_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -47,4 +51,60 @@ func TestJSON(t *testing.T) {
 			t.Errorf("Got %s, expected %s", contentType, headerValue)
 		}
 	}
+}
+
+func TestGet(t *testing.T) {
+	makeStorage(t)
+	defer cleanupStorage(t)
+
+	kvStore := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key4": "value4",
+	}
+	encodedStore := map[string]string{}
+	for key, value := range kvStore {
+		encodedKey := base64.URLEncoding.EncodeToString([]byte(key))
+		encodedValue := base64.URLEncoding.EncodeToString([]byte(value))
+		encodedStore[encodedKey] = encodedValue
+	}
+
+	fileContents, _ := json.Marshal(encodedStore)
+	os.WriteFile(StoragePath+"/data.json", fileContents, 0644)
+
+	testCases := []struct {
+		in  string
+		out string
+		err error
+	}{
+		{"key1", "value1", nil},
+		{"key2", "value2", nil},
+		{"key3", "", nil},
+	}
+	for _, test := range testCases {
+		got, err := Get(context.Background(), test.in)
+		if err != test.err {
+			t.Errorf("Received unexpected error: %s", test.err)
+		}
+		if got != test.out {
+			t.Errorf("Got %s, expected %s", got, test.out)
+		}
+	}
+}
+
+func makeStorage(t *testing.T) {
+	err := os.Mkdir("testdata", 0755)
+	if err != nil && !os.IsExist(err) {
+		t.Fatalf("Couldn't create directory testdata: %s", err)
+	}
+
+	StoragePath = "testdata"
+}
+
+func cleanupStorage(t *testing.T) {
+	if err := os.RemoveAll(StoragePath); err != nil {
+		t.Errorf("Failed to delete storage path: %s", StoragePath)
+	}
+
+	StoragePath = "/tmp/kv"
 }
